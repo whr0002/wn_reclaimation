a52abee9ea80ef8a128d22e6adebbd41bc408909
whr0002 <whr0002@gmail.com>
2015-05-04 14:24:28 -0600

    Project Start

---
+++ WN_Reclaimation/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WN_Reclaimation.MvcApplication" Language="C#" %>
---
+++ WN_Reclaimation/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WN_Reclaimation.Startup))]
+namespace WN_Reclaimation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
